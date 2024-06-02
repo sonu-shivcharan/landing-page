a276d3b932eabd85488b1a84272b3d14bd40cb88
Sonu Shivcharan <sonushivcharan@outlook.com>
2024-06-02 10:53:24 +0530

    update src/main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-const selectElem =(elem)=>document.querySelector(elem);
-const header = selectElem("#header");
-const home = selectElem("#home");
-
+const select =(elem)=>document.querySelector(elem);
+const selectAll=(elem)=>document.querySelectorAll(elem);
+const header = select("#header");
+const home = select("#home");
+const elements = selectAll(".headline-container .slide-up-animation")
+for(let elem of elements){
+  elem.style.opacity='0';
+}
+window.onload=()=>{
+  for(let i=0; i<elements.length; i++){
+    elements[i].style.animation=`slideUp ${0.3*(i+1)}s ease 1`
+    elements[i].style.opacity='1';
+   console.log(elements[i].classList)
+  }
+}
 function handleScroll(){
     const homeSectionHeight = home.clientHeight;
     const scrollTop = window.scrollY;
