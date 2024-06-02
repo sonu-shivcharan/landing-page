5e430d7b73534fdb9a769895cbd1f410311f5deb
Sonu Shivcharan <sonushivcharan@outlook.com>
2024-06-02 12:04:38 +0530

    update src/main.js

--- src/main.js
+++ src/main.js
@@ -6,21 +6,28 @@ const elements = selectAll(".headline-container .slide-up-animation")
 for(let elem of elements){
   elem.style.opacity='0';
 }
-window.onload=()=>{
+function slideUpAnimation(){
   for(let i=0; i<elements.length; i++){
-    elements[i].style.animation=`slideUp ${0.3*(i+1)}s ease 1`
+    elements[i].style.animation=`slideUp ${0.3*(i+0.5)}s ease 1`
     elements[i].style.opacity='1';
    console.log(elements[i].classList)
   }
 }
+window.onload=slideUpAnimation;
 function handleScroll(){
     const homeSectionHeight = home.clientHeight;
     const scrollTop = window.scrollY;
     if(scrollTop>=100){
         header.style.display="block";
-    }else{
-        header.style.display="none";
     }
-  
+    else{
+        header.style.display="none";
+      
+    }
+    if(scrollTop==0){
+      console.log('scroll 0')
+        slideUpAnimation();
+    }
+  //window.removeEventListener('scroll', handleScroll);
 }
 window.addEventListener("scroll", handleScroll);
