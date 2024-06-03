6ffefe0b443801998b828254266008031a8c1d62
23521208.acs <23521208.acs@dypvp.edu.in>
2024-06-03 10:04:42 +0000

    added sidenav

--- src/main.js
+++ src/main.js
@@ -2,26 +2,49 @@ const select = (elem)=>document.querySelector(elem);
 const selectAll = (elem)=>document.querySelectorAll(elem);
 const header = select("#header");
 const home = select("#home");
-const elements = selectAll(".headline-container .slide-up-animation")
+const navBar = select("#header #nav-bar");
+const elements = selectAll(".headline-container .slide-up-animation");
+const menuIcon = select("#header .menu-icon");
+const sidenav = select("#side-nav");
+
+
+menuIcon.onclick = () => {
+  sidenav.style.transform = "translateX(0%)";
+};
+document.addEventListener("click", (e) => {
+  if (e.target !== sidenav && e.target !== menuIcon) {
+    sidenav.style.transform = "translateX(-100%)";
+  }
+});
+
 for (let elem of elements) {
   elem.style.opacity = '0';
 }
 function slideUpAnimation() {
   for (let i = 0; i < elements.length; i++) {
-    elements[i].style.animation = `slideUp ${0.3*(i+0.5)}s ease-out 1`
+    elements[i].style.animation = `slideUp ${0.3*(i+1)}s ease-out 1`
     elements[i].style.opacity = '1';
-    console.log(elements[i].classList)
   }
 }
-window.onload = slideUpAnimation;
+
+window.addEventListener("load", slideUpAnimation)
+
+
 function handleScroll() {
-  const homeSectionHeight = home.clientHeight;
+  const title = header.getElementsByTagName("h1")[0];
   const scrollTop = window.scrollY;
+
   if (scrollTop >= 100) {
-    header.style.display = "block";
+    // Show the title and change header styles for scrolled state
+    title.style.display = "block";
+    header.style.backgroundColor = "#fff";
+    header.style.color = "#000";
   } else {
-    header.style.display = "none";
+    // Hide the title and change header styles for non-scrolled state
+    title.style.display = "none";
+    header.style.backgroundColor = "transparent";
+    header.style.color = "#fff";
   }
-  //window.removeEventListener('scroll', handleScroll);
 }
+
 window.addEventListener("scroll", handleScroll);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'], // Customize your sans-serif font
+        'serif': ['Georgia', 'Times New Roman', 'serif'], // Customize your serif font
+        'mono': ['Courier New', 'Courier', 'monospace'], // Customize your monospace font
+      }
+    }
   },
   plugins: [],
 }
-
